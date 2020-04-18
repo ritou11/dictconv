4ed1b5a3ddf829c7a4447474512db7f70e145d2d
haotian <ritou11@gmail.com>
2020-04-18 20:44:53 +0800

    begin yargs

--- index.js
+++ index.js
@@ -5,28 +5,11 @@ const fs = require('fs');
 const yargRoot = require('yargs');
 
 module.exports = yargRoot
-  .option('c', {
-    alias: 'config-file',
-    describe: 'Json file that contains username, md5_password and other infomation.',
-    default: path.join(os.homedir(), '.thunet-reg'),
-    type: 'string',
+  .option('install', {
+    alias: 'i',
+    describe: 'Install the converted dictionary to the system',
+    default: false,
+    type: 'boolean',
   })
-  .command('login [<ip>]', 'Login my current IP',
-    (yargs) => {
-      yargs
-        .positional('ip', {
-          describe: '<ip> Which IP to register.',
-          type: 'string',
-        });
-    },
-    (argv) => {
-      const config = readConfig(argv);
-      const ck = checkConfig(config);
-      if (ck) {
-        console.error(ck);
-        return;
-      }
-      tryLogin(config);
-    })
   .help()
   .parse;
