d1a8b556cbe68949f822fc26070f885c82f53076
haotian <ritou11@gmail.com>
2020-04-22 22:22:31 +0800

    default command

--- index.js
+++ index.js
@@ -11,5 +11,17 @@ module.exports = yargRoot
     default: false,
     type: 'boolean',
   })
+  .command('$0 <stardict>', 'Convert the startdict to MacOSX dictionary.',
+    (yargs) => {
+      yargs
+        .positional('stardict', {
+          describe: '<stardict> The path to the stardict file.',
+          type: 'string',
+        });
+    },
+    (argv) => {
+      console.log(`this command will be run by default ${argv.stardict}`)
+    })
   .help()
+  .alias('h', 'help')
   .parse;
