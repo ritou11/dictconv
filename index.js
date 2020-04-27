f1d2f154c9cf7b47c90342517489b0c4290129a6
haotian <ritou11@gmail.com>
2020-04-27 11:33:34 +0800

    convert xml

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const yargRoot = require('yargs');
-const { fromArchive } = require('./lib/stardict');
+const { fromArchive, fromIfo } = require('./lib/stardict');
 
 module.exports = yargRoot
   .option('install', {
@@ -10,20 +10,40 @@ module.exports = yargRoot
     default: false,
     type: 'boolean',
   })
-  .command('$0 <stardict>', 'Convert the startdict to MacOSX dictionary.',
+  .command('raw <stardict> <destPath>', 'Convert the startdict to xml dictionary.',
     (yargs) => {
       yargs
         .positional('stardict', {
           describe: '<stardict> The path to the stardict file.',
           type: 'string',
+        })
+        .positional('destPath', {
+          describe: '<destPath> The destination path.',
+          type: 'string',
         });
     },
     (argv) => {
-      if (path.extname(argv.stardict) == '.ifo') {
-        console.log(`${path.extname(argv.stardict)} file detected`);
+      if (path.extname(argv.stardict) === '.ifo') {
+        // console.log(`${path.extname(argv.stardict)} file detected`);
+        fromIfo(argv.stardict, argv.destPath);
       } else {
-        fromArchive(argv.stardict);
+        fromArchive(argv.stardict, argv.destPath);
       }
+    })
+  .command('$0 <stardict> <destPath>', 'Convert the startdict to MacOSX dictionary.',
+    (yargs) => {
+      yargs
+        .positional('stardict', {
+          describe: '<stardict> The path to the stardict file.',
+          type: 'string',
+        })
+        .positional('destPath', {
+          describe: '<destPath> The destination path.',
+          type: 'string',
+        });
+    },
+    (argv) => {
+      console.log(argv);
     })
   .help()
   .alias('h', 'help')
