fc0a82e00e7f5c62274359720db629099a867397
haotian <ritou11@gmail.com>
2020-04-27 18:31:36 +0800

    convert

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const yargRoot = require('yargs');
 const { execSync } = require('child_process');
 const { fromArchive, fromIfo } = require('./lib/raw');
 const { buildDictionary } = require('./lib/build');
+const { mkTmpdir } = require('./lib/utils');
 
 module.exports = yargRoot
   .command('raw <stardict> <destPath>', 'Convert the startdict to xml dictionary.',
@@ -51,10 +52,6 @@ module.exports = yargRoot
         });
     },
     async (argv) => {
-      if (!argv.install && !argv.destPath) {
-        console.error('Please provide destination path or --install.');
-        return;
-      }
       const name = argv.name || argv.rawPath.split(path.sep).slice(-1).pop();
       const xmlFile = path.resolve(argv.rawPath, 'Dictionary.xml');
       const cssFile = path.resolve(argv.rawPath, 'Dictionary.css');
@@ -75,8 +72,10 @@ module.exports = yargRoot
         destPath: argv.destPath,
         verbose: argv.verbose,
       });
+      console.log('Built', dict);
       if (argv.install) {
         execSync(`cp -r ${dict} ~/Library/Dictionaries/`);
+        console.log('Installed to ~/Library/Dictionaries/');
       }
     })
   .command('convert <stardict> [<destPath>]', 'Convert the startdict to Mac dictionary.',
@@ -90,19 +89,55 @@ module.exports = yargRoot
           describe: '<destPath> The destination path.',
           type: 'string',
         })
+        .option('name', {
+          alias: 'n',
+          describe: 'The name of dictionary',
+          type: 'string',
+        })
         .option('install', {
           alias: 'i',
           describe: 'Install the converted dictionary to the system',
           default: false,
           type: 'boolean',
+        })
+        .option('verbose', {
+          alias: 'v',
+          describe: 'Print all logs',
+          default: false,
+          type: 'boolean',
         });
     },
     async (argv) => {
-      if (!argv.install && !argv.destPath) {
-        console.error('Please provide destination path or --install.');
+      const rawDest = mkTmpdir('raw');
+      const rawPath = path.extname(argv.stardict) === '.ifo' ? fromIfo(argv.stardict, rawDest) : fromArchive(argv.stardict, rawDest);
+      console.log('Converted raw file path:', rawPath);
+
+      const name = argv.name || rawPath.split(path.sep).slice(-1).pop();
+      const xmlFile = path.resolve(rawPath, 'Dictionary.xml');
+      const cssFile = path.resolve(rawPath, 'Dictionary.css');
+      const plistFile = path.resolve(rawPath, 'DictInfo.plist');
+      if (!fs.existsSync(xmlFile)) {
+        console.error(`${xmlFile} not found.`);
         return;
       }
-      console.log(argv);
+      if (!fs.existsSync(cssFile)) {
+        console.error(`${cssFile} not found.`);
+        return;
+      }
+      if (!fs.existsSync(plistFile)) {
+        console.error(`${plistFile} not found.`);
+        return;
+      }
+      console.log('Start building...');
+      const dict = await buildDictionary(name, xmlFile, cssFile, plistFile, {
+        destPath: argv.destPath,
+        verbose: argv.verbose,
+      });
+      console.log('Built', dict);
+      if (argv.install) {
+        execSync(`cp -r ${dict} ~/Library/Dictionaries/`);
+        console.log('Installed to ~/Library/Dictionaries/');
+      }
     })
   .help()
   .alias('h', 'help')
