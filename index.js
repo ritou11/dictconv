dcbebc5a7a21ae260452179d393ceb4ed856fcda
haotian <ritou11@gmail.com>
2020-04-27 17:54:03 +0800

    build

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const path = require('path');
-// const fs = require('fs');
+const fs = require('fs');
 const yargRoot = require('yargs');
+const { execSync } = require('child_process');
 const { fromArchive, fromIfo } = require('./lib/raw');
+const { buildDictionary } = require('./lib/build');
 
 module.exports = yargRoot
   .command('raw <stardict> <destPath>', 'Convert the startdict to xml dictionary.',
@@ -23,6 +25,60 @@ module.exports = yargRoot
         fromArchive(argv.stardict, argv.destPath);
       }
     })
+  .command('build <rawPath> [<destPath>]', 'Build the Mac dictionary from raw data',
+    (yargs) => {
+      yargs
+        .positional('rawPath', {
+          describe: '<rawPath> The path to the raw data files.',
+          type: 'string',
+        })
+        .option('name', {
+          alias: 'n',
+          describe: 'The name of dictionary',
+          type: 'string',
+        })
+        .option('install', {
+          alias: 'i',
+          describe: 'Install the converted dictionary to the system',
+          default: false,
+          type: 'boolean',
+        })
+        .option('verbose', {
+          alias: 'v',
+          describe: 'Print all logs',
+          default: false,
+          type: 'boolean',
+        });
+    },
+    async (argv) => {
+      if (!argv.install && !argv.destPath) {
+        console.error('Please provide destination path or --install.');
+        return;
+      }
+      const name = argv.name || argv.rawPath.split(path.sep).slice(-1).pop();
+      const xmlFile = path.resolve(argv.rawPath, 'Dictionary.xml');
+      const cssFile = path.resolve(argv.rawPath, 'Dictionary.css');
+      const plistFile = path.resolve(argv.rawPath, 'DictInfo.plist');
+      if (!fs.existsSync(xmlFile)) {
+        console.error(`${xmlFile} not found.`);
+        return;
+      }
+      if (!fs.existsSync(cssFile)) {
+        console.error(`${cssFile} not found.`);
+        return;
+      }
+      if (!fs.existsSync(plistFile)) {
+        console.error(`${plistFile} not found.`);
+        return;
+      }
+      const dict = await buildDictionary(name, xmlFile, cssFile, plistFile, {
+        destPath: argv.destPath,
+        verbose: argv.verbose,
+      });
+      if (argv.install) {
+        execSync(`cp -r ${dict} ~/Library/Dictionaries/`);
+      }
+    })
   .command('convert <stardict> [<destPath>]', 'Convert the startdict to Mac dictionary.',
     (yargs) => {
       yargs
@@ -41,7 +97,7 @@ module.exports = yargRoot
           type: 'boolean',
         });
     },
-    (argv) => {
+    async (argv) => {
       if (!argv.install && !argv.destPath) {
         console.error('Please provide destination path or --install.');
         return;
