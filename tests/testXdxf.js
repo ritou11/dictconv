1582d14327ef0f30fcaaf2ffd53d73ae7d1f138a
haotian <ritou11@gmail.com>
2020-05-21 00:33:04 +0800

    don't escape in xdxf

--- tests/testXdxf.js
+++ tests/testXdxf.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/extensions */
 import { xdxfTransform } from '../lib/xdxf.js';
 
-const xdxf = `<k>line of duty</k>
+const xdxf = `<co><k>line of duty</k>
 <blockquote><co><b>Date:</b> circa 1918</co></blockquote>
-<blockquote><dtrn> <b>:</b> all that is authorized, required, or normally associated with some field of responsibility</dtrn></blockquote>`;
+<blockquote><dtrn> <b>:</b> &quot; all that is authorized, required, or normally associated with some field of responsibility</dtrn></blockquote></co>`;
 
 console.log(xdxfTransform(xdxf));
